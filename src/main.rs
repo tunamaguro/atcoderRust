e904f8dcdb4e98d77684292559e31a8a1fc2d005
tunamaguro <tsu7magu6@gmail.com>
2022-04-06 22:03:42 +0900

    ABC513 C

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    println!("Hello, world!");
+    input! {n:usize,mut k:i64,mut h:[i64;n]}
+    h.sort();
+    h.reverse();
+    for i in h.iter_mut() {
+        if k == 0 {
+            break;
+        }
+        *i = 0;
+        k -= 1;
+    }
+    let ans: i64 = h.iter().sum();
+    println!("{}", ans)
 }
