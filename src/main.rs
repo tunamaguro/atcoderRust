ff6b3e5a6c1b05c856b1d8fede02ead958e26fb8
tunamaguro <tsu7magu6@gmail.com>
2022-05-14 20:46:14 +0900

    ABC156 B

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
 use proconio::input;
 
-// See mPn and mCn
-// https://manabitimes.jp/math/1352
-fn p(m:i128,n:i128)->i128{
-    let mut s=m;
-    for i in 1..n{
-        s*=m-i;
-    }
-    s
-}
-
-fn f(n:i128)->i128{
-    if n==0{
-        1
-    }else{
-        f(n-1)*n
-    }
-}
-
 fn main() {
-    input! {l:i128}
-    let ans=p(l-1,11)/f(11);
-    println!("{}",ans)
+    input! {mut n:i32,k:i32}
+    let mut digit = 0;
+    while n != 0 {
+        n /= k;
+        digit += 1;
+    }
+    println!("{}", digit)
 }
