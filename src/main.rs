95e895d05b9d6d92248adcc8c5a3c6380bcfcfce
tunamaguro <tsu7magu6@gmail.com>
2022-05-26 14:16:51 +0000

    ABC213 C

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use proconio::input;
 
 fn main() {
-    input! {n:usize,a:[i32;n]}
-    let mut ans = a.iter().enumerate().collect::<Vec<_>>();
-    ans.sort_by(|x, y| (-x.1).cmp(&(-y.1)));
-    println!("{}", ans[1].0 + 1)
+    input! {_h:usize,_w:usize,n:usize,point:[(usize,usize);n]}
+    let mut x_blank = point.iter().map(|v| v.0).collect::<Vec<_>>();
+    let mut y_blank = point.iter().map(|v| v.1).collect::<Vec<_>>();
+    x_blank.sort();
+    y_blank.sort();
+    x_blank.dedup();
+    y_blank.dedup();
+    for (x, y) in point {
+        let x_pos = x_blank.binary_search(&x).unwrap() + 1;
+        let y_pos = y_blank.binary_search(&y).unwrap() + 1;
+        println!("{} {}", &x_pos, &y_pos)
+    }
 }
