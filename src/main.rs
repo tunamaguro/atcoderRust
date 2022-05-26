58a260e9fa43b9b9b786aa8a9d0184fb61efd4ee
tunamaguro <tsu7magu6@gmail.com>
2022-05-26 13:33:09 +0000

    ABC252 C

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
+use std::collections::HashMap;
 
 fn main() {
-    input! {mut n:i32,k:i32}
-    let mut digit = 0;
-    while n != 0 {
-        n /= k;
-        digit += 1;
+    input! {n:usize,s:[Chars;n]}
+    let mut l: Vec<usize> = Vec::new();
+    for i in (0..=9).map(|x| std::char::from_digit(x as u32, 10).unwrap()) {
+        let mut map = HashMap::<usize, usize>::new();
+        s.iter()
+            .map(|c| c.iter().position(|x| x == &i).unwrap())
+            .for_each(|x| *map.entry(x).or_insert(0) += 1);
+        let map_vec: Vec<_> = map.into_iter().collect();
+        let max = map_vec
+            .iter()
+            .max_by(|x, y| {
+                if x.1 == y.1 {
+                    return x.0.cmp(&y.0);
+                }
+                x.1.cmp(&y.1)
+            })
+            .unwrap();
+        l.push(max.0 + 10 * (max.1 - 1))
     }
-    println!("{}", digit)
+    let ans = l.iter().min().unwrap();
+    println!("{}", ans)
 }
