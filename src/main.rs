53e92bc77c065004f2a609068c6310362511c67d
tunamaguro <tsu7magu6@gmail.com>
2022-06-25 14:33:32 +0000

    abc257

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
-use proconio::input;
-
+use proconio::{input, marker::Chars};
+use std::cmp::max;
 fn main() {
-    input! {_h:usize,_w:usize,n:usize,point:[(usize,usize);n]}
-    let mut x_blank = point.iter().map(|v| v.0).collect::<Vec<_>>();
-    let mut y_blank = point.iter().map(|v| v.1).collect::<Vec<_>>();
-    x_blank.sort();
-    y_blank.sort();
-    x_blank.dedup();
-    y_blank.dedup();
-    for (x, y) in point {
-        let x_pos = x_blank.binary_search(&x).unwrap() + 1;
-        let y_pos = y_blank.binary_search(&y).unwrap() + 1;
-        println!("{} {}", &x_pos, &y_pos)
+    input! {n:usize,s:Chars,w:[usize;n]}
+    let mut all_adult = 0;
+    let mut all_child = 0;
+    for c in &s {
+        match c {
+            '1' => all_adult += 1,
+            '0' => all_child += 1,
+            _ => unreachable!(),
+        }
     }
+    let mut m: Vec<_> = s.iter().zip(w).collect();
+    m.sort_by(|a, b| {
+        if a.1 != b.1 {
+            a.1.cmp(&b.1)
+        } else {
+            a.0.cmp(b.0)
+        }
+    });
+    let mut adult_count = 0;
+    let mut child_count = 0;
+    let mut ans = 0;
+    let mut before: usize = 0;
+    for (c, w) in &m {
+        if before == *w {
+            match c {
+                '1' => adult_count += 1,
+                '0' => child_count += 1,
+                _ => unreachable!(),
+            }
+        } else {
+            before = *w;
+            let fx = all_adult - adult_count + child_count;
+            ans = max(ans, fx);
+            match c {
+                '1' => adult_count += 1,
+                '0' => child_count += 1,
+                _ => unreachable!(),
+            }
+        }
+    }
+    println!("{}", max(ans, all_child));
 }
