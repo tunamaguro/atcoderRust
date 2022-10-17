ee260c38737d21cea05f30b40698956e484a0b31
tunamaguro <tsu7magu6@gmail.com>
2022-10-17 15:29:58 +0000

    ABC249 B

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,24 @@
 use proconio::{input, marker::Chars};
-use std::cmp::max;
+use std::collections::HashMap;
+
 fn main() {
-    input! {n:usize,s:Chars,w:[usize;n]}
-    let mut all_adult = 0;
-    let mut all_child = 0;
-    for c in &s {
-        match c {
-            '1' => all_adult += 1,
-            '0' => all_child += 1,
-            _ => unreachable!(),
+    input! {s:Chars}
+    let mut memo = HashMap::new();
+    let mut ans = true;
+    for c in s {
+        if memo.contains_key(&c) {
+            ans = false
+        }
+        memo.entry(c).or_insert(true);
+    }
+    let alphabet = (b'a'..=b'z').map(|x| x as char).collect::<Vec<_>>();
+    let balphabet = (b'A'..=b'Z').map(|x| x as char).collect::<Vec<_>>();
+    let mut has_aa = false;
+    for a in alphabet {
+        for aa in &balphabet {
+            has_aa = has_aa || (memo.contains_key(&a) && memo.contains_key(aa))
         }
     }
-    let mut m: Vec<_> = s.iter().zip(w).collect();
-    m.sort_by(|a, b| {
-        if a.1 != b.1 {
-            a.1.cmp(&b.1)
-        } else {
-            a.0.cmp(b.0)
-        }
-    });
-    let mut adult_count = 0;
-    let mut child_count = 0;
-    let mut ans = 0;
-    let mut before: usize = 0;
-    for (c, w) in &m {
-        if before == *w {
-            match c {
-                '1' => adult_count += 1,
-                '0' => child_count += 1,
-                _ => unreachable!(),
-            }
-        } else {
-            before = *w;
-            let fx = all_adult - adult_count + child_count;
-            ans = max(ans, fx);
-            match c {
-                '1' => adult_count += 1,
-                '0' => child_count += 1,
-                _ => unreachable!(),
-            }
-        }
-    }
-    println!("{}", max(ans, all_child));
+    ans = ans && has_aa;
+    println!("{}", if ans { "Yes" } else { "No" })
 }
