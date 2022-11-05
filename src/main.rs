21059d2dd96518543228146a91641aedf4160c78
tunamaguro <tsu7magu6@gmail.com>
2022-11-05 12:15:19 +0000

    ABC276 A

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 use proconio::{input, marker::Chars};
-use std::collections::HashMap;
 
 fn main() {
-    input! {s:Chars}
-    let mut memo = HashMap::new();
-    let mut ans = true;
-    for c in s {
-        if memo.contains_key(&c) {
-            ans = false
-        }
-        memo.entry(c).or_insert(true);
-    }
-    let alphabet = (b'a'..=b'z').map(|x| x as char).collect::<Vec<_>>();
-    let balphabet = (b'A'..=b'Z').map(|x| x as char).collect::<Vec<_>>();
-    let mut has_aa = false;
-    for a in alphabet {
-        for aa in &balphabet {
-            has_aa = has_aa || (memo.contains_key(&a) && memo.contains_key(aa))
+    input! {mut s:Chars}
+    let mut find = false;
+    s.reverse();
+    let target = 'a';
+    for (i, c) in s.iter().enumerate() {
+        if c == &target {
+            println!("{}", s.len() - i);
+            find = true;
+            break;
         }
     }
-    ans = ans && has_aa;
-    println!("{}", if ans { "Yes" } else { "No" })
+    if !find {
+        println!("{}", -1)
+    }
 }
