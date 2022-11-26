f86560791cd8ff261a36e0596a7a7f077a53aff6
tunamaguro <tsu7magu6@gmail.com>
2022-11-26 12:02:24 +0000

    ABC279

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
-use proconio::input;
-use std::collections::HashMap;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {n:usize,m:usize,edges:[(usize,usize);m]}
-    let mut memo = HashMap::<usize, Vec<usize>>::new();
-    for (ai, bi) in edges {
-        let a = memo.entry(ai).or_insert(vec![]);
-        a.push(bi);
-        let b = memo.entry(bi).or_insert(vec![]);
-        b.push(ai);
+    input! {s:Chars}
+    let mut ans = 0;
+    for c in s {
+        if c == 'v' {
+            ans += 1;
+        } else if c == 'w' {
+            ans += 2;
+        }
     }
-
-    for i in 1..=n {
-        let mut connect_city: Vec<usize> = match memo.get(&i) {
-            Some(arr) => arr.to_vec(),
-            None => Vec::<usize>::new(),
-        };
-        let mut ans = vec![connect_city.len()];
-        connect_city.sort();
-        ans.append(&mut connect_city);
-        let ans = ans
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>()
-            .join(" ");
-        println!("{}", ans)
-    }
+    println!("{}", ans)
 }
