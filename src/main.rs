5ed415f395373a0c697afa973815e040da4b7412
tunamaguro <tsu7magu6@gmail.com>
2022-11-26 12:15:57 +0000

    ABC279 B

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {s:Chars}
-    let mut ans = 0;
-    for c in s {
-        if c == 'v' {
-            ans += 1;
-        } else if c == 'w' {
-            ans += 2;
+    input! {s:Chars,t:Chars}
+    let mut ans = "No";
+    for i in 0..s.len() {
+        let mut is_same = true;
+        for (j, _) in t.iter().enumerate() {
+            let si = i + j;
+            if si >= s.len() {
+                // tの探索範囲がsの長さを超えたらbreak
+                is_same = false;
+                break;
+            }
+            if s[si] != t[j] {
+                is_same = false
+            }
+        }
+        if is_same {
+            ans = "Yes"
         }
     }
     println!("{}", ans)
