8c03da3371559c0e4a9b307abb9ca62558213fa6
tunamaguro <tsu7magu6@gmail.com>
2022-11-29 12:36:36 +0000

    ABC041 B

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input! {n:usize,a:[i128;n]}
-    let c: i128 = 10_i128.pow(9) + 7;
-    let mut sum = 0;
-    for ai in &a {
-        sum += ai;
-        sum %= c;
-    }
-    let mut ans = 0;
-    for ai in a {
-        sum -= ai % c;
-        sum %= c;
-        if sum <= 0 {
-            sum += c
-        }
-        ans += (sum * (ai % c)) % c;
-        ans %= c;
-    }
+    input! {a:i128,b:i128,c:i128}
+    let d = 10_i128.pow(9) + 7;
+    let ans = (a % d) * (b % d) * (c % d);
+    let ans = ans % d;
     println!("{}", ans)
 }
