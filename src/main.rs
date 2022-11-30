10329adf42b1ce7030b141c114131ced138e1106
tunamaguro <tsu7magu6@gmail.com>
2022-11-30 12:14:23 +0000

    ABC278 C

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
-    input! {mut h:i32,mut m:i32}
-    loop {
-        let h2 = h / 10;
-        let h1 = h % 10;
-        let m2 = m / 10;
-        let m1 = m % 10;
-
-        let miss_h = h2 * 10 + m2;
-        let miss_m = h1 * 10 + m1;
-        if miss_h <= 23 && miss_m <= 59 {
-            break;
-        }
-        m += 1;
-        if m > 59 {
-            h += 1;
-            m = 0;
-        }
-        if h > 23 {
-            h = 0
+    input! {_n:usize,q:usize}
+    let mut memo = HashSet::<(usize, usize)>::new();
+    for _ in 0..q {
+        input! {t:usize,a:usize,b:usize}
+        match t {
+            1 => {
+                memo.insert((a, b));
+            }
+            2 => {
+                memo.remove(&(a, b));
+            }
+            3 => {
+                if memo.contains(&(a, b)) && memo.contains(&(b, a)) {
+                    println!("Yes")
+                } else {
+                    println!("No")
+                }
+            }
+            _ => unreachable!(),
         }
     }
-    println!("{} {}", h, m)
 }
