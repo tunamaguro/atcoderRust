b5f12d61d146a06d1d55b24149f5a0d57d5b87fe
tunamaguro <tsu7magu6@gmail.com>
2022-11-30 12:37:08 +0000

    ABC278 D

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
 use proconio::input;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
-    input! {_n:usize,q:usize}
-    let mut memo = HashSet::<(usize, usize)>::new();
+    input! {n:usize,a:[i64;n],q:usize}
+    let mut memo = HashMap::<usize, i64>::new();
+    let mut xq = -1;
     for _ in 0..q {
-        input! {t:usize,a:usize,b:usize}
+        input! {t:usize}
         match t {
             1 => {
-                memo.insert((a, b));
+                input! {b:i64}
+                memo.clear();
+                xq = b;
             }
             2 => {
-                memo.remove(&(a, b));
+                input! {i:usize,b:i64}
+                if xq < 0 {
+                    // xqで初期化される前
+                    *memo.entry(i).or_insert(a[i - 1]) += b;
+                } else {
+                    *memo.entry(i).or_insert(xq) += b;
+                }
+                // println!("{:?}", memo)
             }
             3 => {
-                if memo.contains(&(a, b)) && memo.contains(&(b, a)) {
-                    println!("Yes")
+                input! {i:usize}
+                let out = if xq < 0 {
+                    // xqで初期化される前
+                    memo.entry(i).or_insert(a[i - 1])
                 } else {
-                    println!("No")
-                }
+                    memo.entry(i).or_insert(xq)
+                };
+                println!("{}", out)
+                // println!("{:?}", memo)
             }
             _ => unreachable!(),
         }
