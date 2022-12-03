2cc3285255087356a0f97a2c8f7208166d00ba85
tunamaguro <tsu7magu6@gmail.com>
2022-12-03 12:18:06 +0000

    ABC280 C

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {h:usize,_w:usize,s:[Chars;h]}
+    input! {s:Chars,t:Chars}
     let mut ans = 0;
-    for si in s {
-        for c in si {
-            if c == '#' {
-                ans += 1
-            }
+    for i in 0..s.len() {
+        if s[i] != t[i] {
+            ans = i + 1;
+            break;
         }
+    }
+    if ans == 0 {
+        ans = t.len()
     }
     println!("{}", ans)
 }
