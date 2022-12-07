459186019f016b2112abc1f7c549de93411adb27
tunamaguro <tsu7magu6@gmail.com>
2022-12-07 12:18:47 +0000

    ABC272 B

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,24 @@
 use proconio::input;
-use std::collections::HashMap;
-
-fn prime_factorization(n: i128) -> HashMap<i128, i128> {
-    let mut ans = HashMap::new();
-    let mut n = n;
-
-    let mut p: i128 = 2;
-    while p * p <= n {
-        if n % p != 0 {
-            p += 1;
-            continue;
-        }
-        let mut exponent = 0;
-        while n % p == 0 {
-            exponent += 1;
-            n /= p;
-        }
-        ans.insert(p, exponent);
-    }
-    if n != 1 {
-        ans.insert(n, 1);
-    }
-
-    ans
-}
-
-fn is_multiple(n: i128, m: &HashMap<i128, i128>) -> bool {
-    // 因数をすべてn!が持っていたらtrue,そうでなければfalse
-    let mut ok = true;
-    for (k, size) in m {
-        let mut count_div = 0;
-        let p = *k;
-        let mut e = 1;
-        while p.pow(e) <= n {
-            count_div += n / p.pow(e);
-            e += 1;
-        }
-        ok = ok && (count_div >= *size)
-    }
-
-    ok
-}
 
 fn main() {
-    // 理論及び実装参考
-    // https://qiita.com/drken/items/97e37dd6143e33a64c8c
-    // https://mathscience-teach.com/koukoumath-seisuu1-5/
-    // https://algo-method.com/descriptions/119
-    input! {k:i128}
-    let a = prime_factorization(k);
-
-    let mut left: i128 = -1;
-    let mut right = k;
-    while right - left > 1 {
-        let mid = left + (right - left) / 2;
-        if is_multiple(mid, &a) {
-            right = mid
-        } else {
-            left = mid
+    input! {n:usize,m:usize}
+    let mut memo = [[false; 105]; 105];
+    for _ in 0..m {
+        input! {k:usize,x:[usize;k]}
+        for (i, x1) in x.iter().enumerate() {
+            for x2 in x.iter().skip(i) {
+                memo[*x1 - 1][*x2 - 1] = true;
+                memo[*x2 - 1][*x1 - 1] = true;
+            }
         }
     }
-    println!("{}", right)
+    let mut ans = true;
+    for (n1, a) in memo.iter().enumerate() {
+        for n2 in a.iter().take(n).skip(n1) {
+            if !n2 {
+                ans = false
+            }
+        }
+    }
+    println!("{}", if ans { "Yes" } else { "No" })
 }
