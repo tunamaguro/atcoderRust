1fd902d457dbced3071271bf51ac24b8caa9cf56
tunamaguro <tsu7magu6@gmail.com>
2022-12-13 11:46:57 +0000

    ABC273 B

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
+use std::collections::HashSet;
 
 fn main() {
-    input! {n:usize,x:i32,p:[i32;n]}
-    for (i, pi) in p.iter().enumerate() {
-        if pi == &x {
-            println!("{}", i + 1)
+    input! {n:usize,s:[Chars;n]}
+    let mut memo = HashSet::new();
+    let mark = "HDCS".chars().into_iter().collect::<Vec<_>>();
+    let num = "A23456789TJQK".chars().into_iter().collect::<Vec<_>>();
+    let mut ans = true;
+    for si in s {
+        if mark.contains(&si[0]) && num.contains(&si[1]) {
+            if memo.contains(&si) {
+                ans = false
+            }
+            memo.insert(si);
+        } else {
+            ans = false
         }
     }
+    println!("{}", if ans { "Yes" } else { "No" })
 }
