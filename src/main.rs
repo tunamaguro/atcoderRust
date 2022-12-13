b8d9481cad578675fbe0b8b255a40ec93762d3e8
tunamaguro <tsu7magu6@gmail.com>
2022-12-13 11:37:50 +0000

    ABC273 A

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,10 @@
 use proconio::input;
-use std::cmp::max;
 
 fn main() {
-    input! {n:usize,a:[i32;n]}
-    let mut even = vec![];
-    let mut odd = vec![];
-    for ai in a {
-        if ai % 2 == 0 {
-            even.push(ai)
-        } else {
-            odd.push(ai)
+    input! {n:usize,x:i32,p:[i32;n]}
+    for (i, pi) in p.iter().enumerate() {
+        if pi == &x {
+            println!("{}", i + 1)
         }
     }
-    even.sort();
-    odd.sort();
-    even.reverse();
-    odd.reverse();
-
-    let mut ans = -1;
-
-    if even.len() >= 2 {
-        ans = max(ans, even[0] + even[1])
-    }
-    if odd.len() >= 2 {
-        ans = max(ans, odd[0] + odd[1])
-    }
-    println!("{}", ans)
 }
