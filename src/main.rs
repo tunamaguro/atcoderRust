e76526821d2ad33d39e7a251ef38a45fdd6f3b99
tunamaguro <tsu7magu6@gmail.com>
2022-12-14 14:39:22 +0000

    tessoku A02

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use proconio::input;
 
 fn main() {
-    input! {n:usize}
-    println!("{}",n*n)
-
+    input! {n:usize,x:usize,a:[usize;n]}
+    let mut ans = false;
+    for ai in a {
+        if ai == x {
+            ans = true
+        }
+    }
+    println!("{}", if ans { "Yes" } else { "No" })
 }
