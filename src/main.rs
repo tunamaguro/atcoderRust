91068837cadbbe2c5e00f727f4b12b7d0b4f15d2
tunamaguro <tsu7magu6@gmail.com>
2022-12-14 14:37:37 +0000

    tessoku A01

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use proconio::input;
 
 fn main() {
-    input! {n:usize,h:[i32;n]}
-    let ans = h.iter().enumerate().max_by(|a, b| a.1.cmp(b.1)).unwrap();
-    println!("{}", ans.0 + 1)
+    input! {n:usize}
+    println!("{}",n*n)
+
 }
