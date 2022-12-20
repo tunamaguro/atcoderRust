6196d6d769b5b61bb7e8e518335ba6e5cbba9e83
tunamaguro <tsu7magu6@gmail.com>
2022-12-20 12:52:16 +0000

    ABC282 B

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {k:usize}
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars();
-    let ans: String = alphabet.take(k).collect();
+    input! {n:usize,m:usize,s:[Chars;n]}
+    let mut ans = 0;
+    for a in 0..n {
+        for b in a..n {
+            if a == b {
+                continue;
+            }
+            let mut c = 0;
+            for j in 0..m {
+                if s[a][j] == 'o' || s[b][j] == 'o' {
+                    c += 1;
+                }
+            }
+            if c == m {
+                ans += 1
+            }
+        }
+    }
     println!("{}", ans)
 }
