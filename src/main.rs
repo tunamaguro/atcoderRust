da1bf4b3e8f2b1ca25704990e9ff2f73dc2fa586
tunamaguro <tsu7magu6@gmail.com>
2022-12-20 12:45:16 +0000

    ABC282 A

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use proconio::input;
 
 fn main() {
-    input! {n:usize,x:usize,a:[usize;n]}
-    let mut ans = false;
-    for ai in a {
-        if ai == x {
-            ans = true
-        }
-    }
-    println!("{}", if ans { "Yes" } else { "No" })
+    input! {k:usize}
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars();
+    let ans: String = alphabet.take(k).collect();
+    println!("{}", ans)
 }
