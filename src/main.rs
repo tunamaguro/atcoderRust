0074f12a268f5bc892bf67216c76f7f8cb6e74af
tunamaguro <tsu7magu6@gmail.com>
2023-01-07 12:48:50 +0000

    ABC284 C ref https://algo-logic.info/connected-components-num/

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
+use std::vec;
+
 use proconio::input;
 
-fn main() {
-    input! {t:usize}
-    for _ in 0..t {
-        input! {n:usize,a:[i32;n]}
-        let mut ans = 0;
-        for ai in a {
-            if ai % 2 == 1 {
-                ans += 1;
-            }
+fn dfs(graph: &Vec<Vec<bool>>, seen: &mut Vec<bool>, v: usize) {
+    seen[v] = true;
+    for (i, e) in graph[v].iter().enumerate() {
+        if !seen[i] && *e {
+            dfs(graph, seen, i)
         }
-        println!("{}", ans)
     }
+}
+
+fn main() {
+    input! {n:usize,m:usize}
+    let mut edges = vec![vec![false; n]; n];
+    for _ in 0..m {
+        input! {u:usize,v:usize}
+        let (u, v) = (u - 1, v - 1);
+        edges[u][v] = true;
+        edges[v][u] = true;
+    }
+    let mut seen = vec![false; n];
+    let mut ans = 0;
+    for i in 0..n {
+        if !seen[i] {
+            dfs(&edges, &mut seen, i);
+            ans += 1;
+        }
+    }
+    println!("{:?}", ans)
 }
