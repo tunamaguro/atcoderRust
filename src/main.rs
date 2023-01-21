09ee7be8383399b6133629e8ca3e3f7051d8dc6d
tunamaguro <tsu7magu6@gmail.com>
2023-01-21 08:36:54 +0000

    ABC283 C

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {n:usize,mut a:[i32;n],q:usize}
-    for _ in 0..q {
-        input! {u:usize}
-        match u {
-            1 => {
-                input! {k:usize,x:i32}
-                a[k - 1] = x
-            }
-            2 => {
-                input! {k:usize}
-                println!("{}", a[k - 1])
-            }
-            _ => unreachable!(),
+    input! {s:Chars}
+    let mut ans = 0;
+    let mut i: usize = 0;
+    while s.len() > i {
+        if s.len() > i + 1 && s[i] == '0' && s[i + 1] == '0' {
+            i += 1;
         }
+        ans += 1;
+
+        i += 1;
     }
+    println!("{}", ans)
 }
