f2e6341ce36bec485c5e167cbe9f7acc9e7dcd1f
tunamaguro <tsu7magu6@gmail.com>
2023-01-21 12:45:22 +0000

    ABC286 C

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,27 @@
 use proconio::{input, marker::Chars};
+use std::cmp::min;
+use std::collections::VecDeque;
 
-fn main() {
-    input! {_n:usize,s:Chars}
-    let mut ans: String = "".to_string();
-
-    let mut nya = false;
-    for si in s {
-        match si {
-            'n' => {
-                nya = true;
-                ans = format!("{}{}", ans, si.to_string());
-            }
-            'a' => {
-                if nya {
-                    ans = format!("{}{}", ans, "ya");
-                } else {
-                    ans = format!("{}{}", ans, si.to_string());
-                }
-                nya = false
-            }
-            _ => {
-                nya = false;
-                ans = format!("{}{}", ans, si.to_string());
-            }
+fn check_rotate(s: &VecDeque<&char>) -> u128 {
+    let n = s.len();
+    let mut ans = 0;
+    for i in 0..(n / 2 + n % 2) {
+        if s[i] != s[n - i - 1] {
+            ans += 1;
         }
     }
+    ans
+}
 
+fn main() {
+    input! {n:usize,a:u128,b:u128,mut s:Chars}
+    let mut s = s.iter().collect::<VecDeque<_>>();
+    let mut ans: u128 = check_rotate(&s) * b;
+    for i in 1..=n {
+        let t = s.pop_front().unwrap();
+        s.push_back(t);
+
+        ans = min(ans, i as u128 * a + check_rotate(&s) * b)
+    }
     println!("{}", ans)
 }
