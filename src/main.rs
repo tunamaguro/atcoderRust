3effe99200dd4f17795758cb214ba39e0a65a25a
tunamaguro <tsu7magu6@gmail.com>
2023-01-21 08:24:27 +0000

    ABC283 B

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use proconio::input;
 
 fn main() {
-    input! {a:usize,b:usize}
-    println!("{}", a.pow(std::convert::TryInto::try_into(b).unwrap()))
+    input! {n:usize,mut a:[i32;n],q:usize}
+    for _ in 0..q {
+        input! {u:usize}
+        match u {
+            1 => {
+                input! {k:usize,x:i32}
+                a[k - 1] = x
+            }
+            2 => {
+                input! {k:usize}
+                println!("{}", a[k - 1])
+            }
+            _ => unreachable!(),
+        }
+    }
 }
