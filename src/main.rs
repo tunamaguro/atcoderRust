c42ecb7c49232eb97e49b4f5083a672e43b09008
tunamaguro <tsu7magu6@gmail.com>
2023-01-28 12:02:55 +0000

    ABC287 A

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,14 @@
-use proconio::{input, marker::Chars};
-use std::cmp::min;
-use std::collections::VecDeque;
-
-fn check_rotate(s: &VecDeque<&char>) -> u128 {
-    let n = s.len();
-    let mut ans = 0;
-    for i in 0..(n / 2 + n % 2) {
-        if s[i] != s[n - i - 1] {
-            ans += 1;
-        }
-    }
-    ans
-}
+use proconio::input;
 
 fn main() {
-    input! {n:usize,a:u128,b:u128,mut s:Chars}
-    let mut s = s.iter().collect::<VecDeque<_>>();
-    let mut ans: u128 = check_rotate(&s) * b;
-    for i in 1..=n {
-        let t = s.pop_front().unwrap();
-        s.push_back(t);
-
-        ans = min(ans, i as u128 * a + check_rotate(&s) * b)
+    input! {n:usize,s:[String;n]}
+    let mut a = 0;
+    for si in s {
+        if &si == "For" {
+            a += 1;
+        }
     }
-    println!("{}", ans)
+
+    let ans = a >= (n / 2 + n % 2);
+    println!("{}", if ans { "Yes" } else { "No" })
 }
