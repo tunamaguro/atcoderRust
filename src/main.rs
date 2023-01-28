23828f2051c9f2cdde23bab73d63a5232d102fd0
tunamaguro <tsu7magu6@gmail.com>
2023-01-28 12:10:39 +0000

    ABC287 B

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use proconio::input;
 
 fn main() {
-    input! {n:usize,s:[String;n]}
-    let mut a = 0;
+    input! {n:usize,m:usize,s:[i32;n],t:[i32;m]}
+    let mut ans = 0;
     for si in s {
-        if &si == "For" {
-            a += 1;
+        let s1 = si % 10;
+        let s2 = (si - s1) % 100;
+        let s3 = (si - s1 - s2) % 1000;
+        let a = [s3, s2, s1];
+        for ti in &t {
+            let t1 = ti % 10;
+            let t2 = (ti - t1) % 100;
+            let t3 = (ti - t1 - t2) % 1000;
+            let b = [t3, t2, t1];
+            if a == b {
+                ans += 1;
+                break;
+            }
         }
     }
 
-    let ans = a >= (n / 2 + n % 2);
-    println!("{}", if ans { "Yes" } else { "No" })
+    println!("{}", ans)
 }
