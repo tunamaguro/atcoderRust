be59b2ca9de956cbf60bd10f5d0b8731710baf66
tunamaguro <tsu7magu6@gmail.com>
2023-02-04 11:36:52 +0000

    add petgraph

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
 use proconio::input;
 
-fn main() {
-    input! {n:usize,m:usize,s:[i32;n],t:[i32;m]}
-    let mut ans = 0;
-    for si in s {
-        let s1 = si % 10;
-        let s2 = (si - s1) % 100;
-        let s3 = (si - s1 - s2) % 1000;
-        let a = [s3, s2, s1];
-        for ti in &t {
-            let t1 = ti % 10;
-            let t2 = (ti - t1) % 100;
-            let t3 = (ti - t1 - t2) % 1000;
-            let b = [t3, t2, t1];
-            if a == b {
-                ans += 1;
-                break;
-            }
+fn dfs(g: &Vec<Vec<bool>>, seen: &mut Vec<bool>, to: usize) {
+    seen[to] = true;
+    for (next, conn) in g[to].iter().enumerate() {
+        if !conn {
+            continue;
         }
+        if seen[next] {
+            continue;
+        }
+
+        dfs(g, seen, next);
+    }
+}
+
+fn main() {
+    input! {n:usize,m:usize,edges:[(usize,usize);m]}
+    let mut graph = vec![vec![false; n]; n];
+    for (u, v) in &edges {
+        let u = u - 1;
+        let v = v - 1;
+        graph[u][v] = true;
+        graph[v][u] = true;
+    }
+    let mut seen = vec![false; n];
+
+    let mut c = 0;
+
+    for i in 0..n {
+        if seen[i] {
+            continue;
+        }
+        dfs(&graph, &mut seen, i);
+        c += 1;
     }
 
-    println!("{}", ans)
+    let ans = c == 1
+        && edges.len() == (n - 1)
+        && graph
+            .iter()
+            .map(|x| x.iter().filter(|x| **x).count())
+            .all(|x| x <= 2);
+
+    println!("{}", if ans { "Yes" } else { "No" })
 }
