fa4045e857f8d44101fa7d1ef5b0fe1502310ab2
tunamaguro <tsu7magu6@gmail.com>
2023-02-04 12:23:22 +0000

    ABC288 C

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use petgraph::unionfind::UnionFind;
 use proconio::input;
 
 fn main() {
-    input! {n:usize,p:[(i32,i32);n]}
-    for (a, b) in p {
-        println!("{}", a + b)
+    input! {n:usize,m:usize,edges:[(usize,usize);m]}
+
+    let mut graph = UnionFind::<usize>::new(n);
+
+    let mut ans = 0;
+    for (a, b) in edges {
+        let (a, b) = (a - 1, b - 1);
+        if graph.equiv(a, b) {
+            ans += 1;
+        }
+        graph.union(a, b);
     }
+    println!("{}", ans)
 }
