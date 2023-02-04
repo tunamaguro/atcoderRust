cd7e9bf2b083593401e7ec4943c3839697c4e5e8
tunamaguro <tsu7magu6@gmail.com>
2023-02-04 12:03:47 +0000

    ABC288 A

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 use proconio::input;
 
 fn main() {
-    input! {n:usize,a:[usize;n]}
-    let mut memo = vec![0];
-    for ai in a {
-        let ai = ai - 1;
-        let generation = memo[ai] + 1;
-        memo.push(generation);
-        memo.push(generation);
-    }
-    for g in memo {
-        println!("{}", g)
+    input! {n:usize,p:[(i32,i32);n]}
+    for (a, b) in p {
+        println!("{}", a + b)
     }
 }
