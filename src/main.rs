c3756c9aae0a31e237b1689fff8be3f502849d87
tunamaguro <tsu7magu6@gmail.com>
2023-02-05 06:52:43 +0000

    三井住友信託銀行プログラミングコンテスト2019 B

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,11 @@
-use proconio::{input, marker::Chars};
+use proconio::input;
 
 fn main() {
-    input! {_n:usize,a:i32,b:i32,s:Chars}
-
-    let mut a_num = 0;
-    let mut b_num = 0;
-
-    for si in s {
-        let tmp = (a + b) > (a_num + b_num);
-        match si {
-            'a' => {
-                if tmp {
-                    println!("Yes");
-                    a_num += 1;
-                } else {
-                    println!("No");
-                }
-            }
-            'b' => {
-                if tmp && b > b_num {
-                    println!("Yes");
-                    b_num += 1;
-                } else {
-                    println!("No");
-                }
-            }
-            _ => {
-                println!("No");
-            }
-        }
+    input! {n:f32}
+    let maybe_ans = (n / 1.08).ceil();
+    if (maybe_ans * 1.08).floor() == n {
+        println!("{}", maybe_ans);
+    } else {
+        println!(":(");
     }
 }
