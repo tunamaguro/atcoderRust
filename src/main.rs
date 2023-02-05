1629c1752cdcc6ab89178f0678bad0531edbeb11
tunamaguro <tsu7magu6@gmail.com>
2023-02-05 06:59:30 +0000

    ABC121 B

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use proconio::input;
 
 fn main() {
-    input! {n:f32}
-    let maybe_ans = (n / 1.08).ceil();
-    if (maybe_ans * 1.08).floor() == n {
-        println!("{}", maybe_ans);
-    } else {
-        println!(":(");
+    input! {n:usize,m:usize,c:i32,b:[i32;m], codes:[[i32;m];n]}
+    let mut ans = 0;
+    for code in codes {
+        let tmp: i32 = code.iter().zip(b.iter()).map(|(a, b)| a * b).sum();
+        if tmp + c > 0 {
+            ans += 1;
+        }
     }
+    println!("{}", ans)
 }
