abb1f39506f8f1892cbf21077981681be0286700
tunamaguro <tsu7magu6@gmail.com>
2023-02-11 12:06:43 +0000

    ABC289 A

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {mut s:Chars}
-    let mut ans = 2;
-    for _ in 0..s.len() {
-        s.pop();
-        let si = s.len();
-        if si % 2 == 0 {
-            let si = si / 2;
-            let s1 = &s[..si];
-            let s2 = &s[si..];
-            if s1 == s2 {
-                ans = std::cmp::max(ans, si * 2);
+    input! {s:Chars}
+    let ans = s
+        .iter()
+        .map(|&c| {
+            if c == '0' {
+                '1'.to_string()
+            } else {
+                '0'.to_string()
             }
-        }
-    }
+        })
+        .collect::<Vec<_>>();
+
+    let ans = ans.join("");
+
     println!("{}", ans)
 }
