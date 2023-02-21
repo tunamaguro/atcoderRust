666d64ebff59cbfe20e324c933eab297c1a63946
tunamaguro <tsu7magu6@gmail.com>
2023-02-21 12:51:40 +0000

    ABC098 B

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,22 @@
 use proconio::{input, marker::Chars};
-use std::cmp::min;
+use std::collections::HashSet;
+
 fn main() {
-    input! {h:usize,w:usize, s:[Chars;h]}
-    let mut ans = vec![];
-    for hi in 0..h {
-        let mut r = vec![];
-        for wi in 0..w {
-            let hu = if hi == 0 { hi } else { hi - 1 };
-            let hl = min(hi + 1, h - 1);
-            let wl = if wi == 0 { wi } else { wi - 1 };
-            let wr = min(wi + 1, w - 1);
+    input! {n:usize,s:Chars}
+    let mut ans = 0;
+    for i in 0..=n {
+        let x = &s[0..i];
+        let y = &s[(i)..n];
+        let k = x.iter().collect::<HashSet<_>>();
+        let l = y.iter().collect::<HashSet<_>>();
 
-            if s[hi][wi] == '#' {
-                r.push('#'.to_string());
-                continue;
-            }
-
-            let mut c = 0;
-            for a in hu..=hl {
-                for b in wl..=wr {
-                    if a == hi && b == wi {
-                        continue;
-                    }
-                    if s[a][b] == '#' {
-                        c += 1;
-                    }
-                }
-            }
-            r.push(format!("{}", c))
+        let mut c = 0;
+        for p in l {
+            if k.contains(&p) {
+                c += 1;
+            };
         }
-        ans.push(r);
+        ans = std::cmp::max(c, ans);
     }
-
-    for r in ans {
-        println!("{}", r.join(""))
-    }
+    println!("{}", ans)
 }
