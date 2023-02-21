eb931318bc665ba7804481f6c52d1cf2a3b5a16d
tunamaguro <tsu7magu6@gmail.com>
2023-02-21 12:41:36 +0000

    ABC075 B

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-use proconio::input;
-
+use proconio::{input, marker::Chars};
+use std::cmp::min;
 fn main() {
-    input! {n:usize,k:usize,mut a:[i32;n]}
-    a.sort();
-    a.dedup();
-    let mut ans = 0;
-    for (i, &ai) in a.iter().enumerate() {
-        if i != ai as usize {
-            break;
+    input! {h:usize,w:usize, s:[Chars;h]}
+    let mut ans = vec![];
+    for hi in 0..h {
+        let mut r = vec![];
+        for wi in 0..w {
+            let hu = if hi == 0 { hi } else { hi - 1 };
+            let hl = min(hi + 1, h - 1);
+            let wl = if wi == 0 { wi } else { wi - 1 };
+            let wr = min(wi + 1, w - 1);
+
+            if s[hi][wi] == '#' {
+                r.push('#'.to_string());
+                continue;
+            }
+
+            let mut c = 0;
+            for a in hu..=hl {
+                for b in wl..=wr {
+                    if a == hi && b == wi {
+                        continue;
+                    }
+                    if s[a][b] == '#' {
+                        c += 1;
+                    }
+                }
+            }
+            r.push(format!("{}", c))
         }
-        if i >= k {
-            break;
-        }
-        ans = ai + 1;
+        ans.push(r);
     }
-    println!("{}", ans)
+
+    for r in ans {
+        println!("{}", r.join(""))
+    }
 }
