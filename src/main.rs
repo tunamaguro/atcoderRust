2bc3d73d784c03b6f3399ca04874816351ef547e
tunamaguro <tsu7magu6@gmail.com>
2023-02-24 14:04:33 +0000

    ABC060 B

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,11 @@
 use proconio::input;
 
+// ka=c mod b
+// ka-c=lm
+
 fn main() {
-    input! {a:[[i32;3];3],n:usize,b:[i32;n]}
+    input! {a:i32,b:i32,c:i32}
+    let ans = (0..110).map(|x| x * a).any(|x| x % b == c);
 
-    let mut bingo = [[false; 3]; 3];
-
-    for i in b {
-        for h in 0..3 {
-            for w in 0..3 {
-                bingo[h][w] |= a[h][w] == i
-            }
-        }
-    }
-
-    let mut ans = false;
-    for i in 0..3 {
-        let w_bingo = bingo[i].iter().all(|x| *x);
-        let h_bingo = bingo.iter().map(|w| w[i]).all(|x| x);
-        ans |= w_bingo || h_bingo;
-    }
-    let rd_bingo = (0..3).map(|i| bingo[i][i]).all(|x| x);
-    let ld_bingo = (0..3)
-        .into_iter()
-        .zip((0..3).rev())
-        .map(|(h, w)| bingo[h][w])
-        .all(|x| x);
-    ans |= rd_bingo || ld_bingo;
-    println!("{}", if ans { "Yes" } else { "No" })
+    println!("{}", if ans { "YES" } else { "NO" })
 }
