2429231069120830ba17bc993e4b0ce7f4e54347
tunamaguro <tsu7magu6@gmail.com>
2023-02-26 12:06:47 +0000

    ABC291 A

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-use proconio::input;
-
-fn manhattan(p1: (i32, i32), p2: (i32, i32)) -> i32 {
-    (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()
-}
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {n:usize,m:usize,s:[(i32,i32);n],g:[(i32,i32);m]}
-    let ans = s
-        .iter()
-        .map(|&p1| g.iter().map(move |&p2| manhattan(p1, p2)))
-        .map(|x| x.collect::<Vec<_>>())
-        // .map(|x| x.rev().collect::<Vec<_>>())
-        // .map(|x| *x.iter().min_by_key(|&d| d).unwrap())
-        .collect::<Vec<_>>();
-
-    for i in 0..n {
-        let m = ans[i].iter().min().unwrap();
-        let id = ans[i].iter().position(|x| x == m).unwrap() + 1;
-        println!("{}", id)
+    input! {s:Chars}
+    let a: Vec<_> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .into_iter()
+        .collect_vec();
+    for (i, c) in s.iter().enumerate() {
+        if a.contains(c) {
+            println!("{}", i + 1);
+            return;
+        }
     }
 }
