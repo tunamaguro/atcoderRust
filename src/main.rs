ab0d0c6ab9eb0864ef451575460857ca894ff740
tunamaguro <tsu7magu6@gmail.com>
2023-02-26 12:15:40 +0000

    ABC291 B

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use itertools::Itertools;
-use proconio::{input, marker::Chars};
+use std::collections::VecDeque;
+
+use proconio::input;
 
 fn main() {
-    input! {s:Chars}
-    let a: Vec<_> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        .chars()
-        .into_iter()
-        .collect_vec();
-    for (i, c) in s.iter().enumerate() {
-        if a.contains(c) {
-            println!("{}", i + 1);
-            return;
-        }
+    input! {n:usize,mut x:[i32;5*n]}
+    x.sort();
+    let mut x = x.into_iter().collect::<VecDeque<_>>();
+    for _ in 0..n {
+        x.pop_back();
+        x.pop_front();
     }
+    let ans = x.iter().map(|&x| x as f64).sum::<f64>() / (3.0 * n as f64);
+    println!("{}", ans)
 }
