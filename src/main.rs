607a5b4dfa4a5854b902cf36ba3e49d7da80f35d
tunamaguro <tsu7magu6@gmail.com>
2023-02-28 15:14:25 +0000

    ABC096 C

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,30 @@
-use itertools::Itertools;
 use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {h:usize,w:usize, a:[Chars;h]}
-    let mut w_blank = vec![];
+    input! {h:usize,w:usize, s:[Chars;h]}
+    let p = '#';
 
+    let mut ans = true;
     for i in 0..h {
-        let mut b = 0;
         for j in 0..w {
-            if a[i][j] == '.' {
-                b += 1;
-            }
-        }
-        if b == w {
-            w_blank.push(i);
-        }
-    }
-
-    let mut h_blank = vec![];
-
-    for j in 0..w {
-        let mut b = 0;
-        for i in 0..h {
-            if a[i][j] == '.' {
-                b += 1;
-            }
-        }
-        if b == h {
-            h_blank.push(j)
-        }
-    }
-
-    let mut ans = vec![];
-    for i in 0..h {
-        if w_blank.contains(&i) {
-            continue;
-        }
-        let mut r = vec![];
-        for j in 0..w {
-            if h_blank.contains(&j) {
+            if s[i][j] != p {
                 continue;
             }
-            r.push(a[i][j])
+            let mut con = false;
+            if i > 0 {
+                con |= s[i - 1][j] == p;
+            }
+            if i < (h - 1) {
+                con |= s[i + 1][j] == p
+            }
+            if j > 0 {
+                con |= s[i][j - 1] == p;
+            }
+            if j < (w - 1) {
+                con |= s[i][j + 1] == p
+            }
+            ans &= con;
         }
-        ans.push(r);
     }
-    let ans = ans.iter().map(|r| r.iter().map(|x| x.to_string()).join(""));
-    for a in ans {
-        println!("{}", a);
-    }
+    println!("{}", if ans { "Yes" } else { "No" })
 }
