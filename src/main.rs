a6c52f177adf848728e4b84da17e96011eefdf3a
tunamaguro <tsu7magu6@gmail.com>
2023-02-28 15:04:52 +0000

    ABC107 B

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,52 @@
+use itertools::Itertools;
 use proconio::{input, marker::Chars};
-use std::collections::HashSet;
 
 fn main() {
-    input! {_n:usize,s:Chars}
-    let mut memo = HashSet::new();
-    let mut point = (0, 0);
-    memo.insert(point);
-    for c in s {
-        match c {
-            'L' => {
-                point = (point.0 - 1, point.1);
+    input! {h:usize,w:usize, a:[Chars;h]}
+    let mut w_blank = vec![];
+
+    for i in 0..h {
+        let mut b = 0;
+        for j in 0..w {
+            if a[i][j] == '.' {
+                b += 1;
             }
-            'R' => {
-                point = (point.0 + 1, point.1);
-            }
-            'U' => {
-                point = (point.0, point.1 + 1);
-            }
-            'D' => {
-                point = (point.0, point.1 - 1);
-            }
-            _ => unreachable!(),
         }
-        if memo.contains(&point) {
-            println!("Yes");
-            return;
+        if b == w {
+            w_blank.push(i);
         }
-        memo.insert(point);
     }
-    println!("No")
+
+    let mut h_blank = vec![];
+
+    for j in 0..w {
+        let mut b = 0;
+        for i in 0..h {
+            if a[i][j] == '.' {
+                b += 1;
+            }
+        }
+        if b == h {
+            h_blank.push(j)
+        }
+    }
+
+    let mut ans = vec![];
+    for i in 0..h {
+        if w_blank.contains(&i) {
+            continue;
+        }
+        let mut r = vec![];
+        for j in 0..w {
+            if h_blank.contains(&j) {
+                continue;
+            }
+            r.push(a[i][j])
+        }
+        ans.push(r);
+    }
+    let ans = ans.iter().map(|r| r.iter().map(|x| x.to_string()).join(""));
+    for a in ans {
+        println!("{}", a);
+    }
 }
