4383ebd8ef49252231327a0115d68a34d1d9db78
tunamaguro <tsu7magu6@gmail.com>
2023-03-11 12:12:17 +0000

    ABC293 B

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use proconio::{input, marker::Chars};
+use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
-    input! {mut s:Chars}
-    let n = s.len() / 2;
-    for i in 0..n {
-        s.swap(2 * i + 1, 2 * i);
+    input! {n:usize,a:[usize;n]}
+    let mut memo = HashSet::new();
+    for i in 1..=n {
+        memo.insert(i);
     }
+    for (i, ai) in a.iter().enumerate() {
+        let i = i + 1;
+        if memo.contains(&i) {
+            memo.remove(ai);
+        }
+    }
+
+    let mut ans: Vec<_> = memo.iter().collect();
+    println!("{}", ans.len());
+    ans.sort();
     println!(
         "{}",
-        s.iter().map(|x| x.to_string()).collect::<Vec<_>>().join("")
+        ans.iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
     )
 }
