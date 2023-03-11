de0c5799e01dbf25b1e2d59aea5ac57bcf2fa145
tunamaguro <tsu7magu6@gmail.com>
2023-03-11 12:38:59 +0000

    ABC293 C

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 use proconio::input;
-use std::collections::HashSet;
 
-fn main() {
-    input! {n:usize,a:[usize;n]}
-    let mut memo = HashSet::new();
-    for i in 1..=n {
-        memo.insert(i);
-    }
-    for (i, ai) in a.iter().enumerate() {
-        let i = i + 1;
-        if memo.contains(&i) {
-            memo.remove(ai);
+fn check(board: &Vec<Vec<i32>>, ans: &mut i32, x: usize, y: usize, mut memo: Vec<i32>) {
+    // println!("x = {} y = {}", x, y);
+
+    let now = board[y][x];
+    for i in &memo {
+        if *i == now {
+            return;
         }
     }
 
-    let mut ans: Vec<_> = memo.iter().collect();
-    println!("{}", ans.len());
-    ans.sort();
-    println!(
-        "{}",
-        ans.iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>()
-            .join(" ")
-    )
+    if y == board.len() - 1 && x == board[0].len() - 1 {
+        *ans += 1;
+        return;
+    }
+
+    memo.push(now);
+    let rd = [(1, 0), (0, 1)];
+    for (xp, yp) in &rd {
+        let xn = x + xp;
+        let yn = y + yp;
+        if xn < board[0].len() && yn < board.len() {
+            check(board, ans, xn, yn, memo.clone())
+        }
+    }
+}
+
+fn main() {
+    input! {h:usize,w:usize,a:[[i32;w];h]}
+
+    let mut ans = 0;
+    check(&a, &mut ans, 0, 0, vec![]);
+    println!("{}", ans)
 }
