398b73765bcb9f867e81225aab72cfc42255850b
tunamaguro <tsu7magu6@gmail.com>
2023-03-11 12:04:29 +0000

    ABC293 A

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,13 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {n:i32}
-
-    let mut ans = 0;
-    for x in 1..n {
-        let y = n - x;
-        // println!("x = {} y = {}", x, y);
-        let (mut a, mut b) = (0, 0);
-
-        for i in 1..=x {
-            if i * i > x {
-                break;
-            }
-            if x % i == 0 {
-                a += 1;
-                if x != i * i {
-                    a += 1;
-                }
-            }
-        }
-
-        for i in 1..=y {
-            if i * i > y {
-                break;
-            }
-            if y % i == 0 {
-                b += 1;
-                if y != i * i {
-                    b += 1;
-                }
-            }
-        }
-
-        // println!("a = {} b = {}", a, b);
-
-        ans += a * b;
+    input! {mut s:Chars}
+    let n = s.len() / 2;
+    for i in 0..n {
+        s.swap(2 * i + 1, 2 * i);
     }
-    println!("{}", ans)
+    println!(
+        "{}",
+        s.iter().map(|x| x.to_string()).collect::<Vec<_>>().join("")
+    )
 }
