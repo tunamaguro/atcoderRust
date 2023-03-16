7fcb444684a0218d1e763053ac88959ba76bbd2e
tunamaguro <tsu7magu6@gmail.com>
2023-03-16 11:39:00 +0000

    ABC124 C

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,26 @@
-use proconio::input;
-
-fn check(board: &Vec<Vec<i32>>, ans: &mut i32, x: usize, y: usize, mut memo: Vec<i32>) {
-    // println!("x = {} y = {}", x, y);
-
-    let now = board[y][x];
-    for i in &memo {
-        if *i == now {
-            return;
-        }
-    }
-
-    if y == board.len() - 1 && x == board[0].len() - 1 {
-        *ans += 1;
-        return;
-    }
-
-    memo.push(now);
-    let rd = [(1, 0), (0, 1)];
-    for (xp, yp) in &rd {
-        let xn = x + xp;
-        let yn = y + yp;
-        if xn < board[0].len() && yn < board.len() {
-            check(board, ans, xn, yn, memo.clone())
-        }
-    }
-}
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {h:usize,w:usize,a:[[i32;w];h]}
-
-    let mut ans = 0;
-    check(&a, &mut ans, 0, 0, vec![]);
+    input! {s:Chars}
+    let mut wb = 0;
+    let mut bw = 0;
+    for (i, &c) in s.iter().enumerate() {
+        match (i % 2, c) {
+            (0, '0') => {
+                wb += 1;
+            }
+            (1, '0') => {
+                bw += 1;
+            }
+            (0, '1') => {
+                bw += 1;
+            }
+            (1, '1') => {
+                wb += 1;
+            }
+            _ => unreachable!(),
+        }
+    }
+    let ans = std::cmp::min(wb, bw);
     println!("{}", ans)
 }
