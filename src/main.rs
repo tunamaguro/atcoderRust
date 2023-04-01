e560e7de7369c093ca33844bc7f5c0e466d92b93
tunamaguro <tsu7magu6@gmail.com>
2023-04-01 12:02:47 +0000

    ABC296 A

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,14 @@
-use proconio::input;
-use std::cmp::Ordering;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {n:usize,h:[i32;n]}
-    // 0="=" 1="<" -1=">"
-    let mut a = 0;
-    let mut b = h[0];
-    let mut memo = vec![h[0]];
-    for i in h {
-        match i.cmp(&b) {
-            Ordering::Equal => {}
-            Ordering::Greater => {
-                if a == -1 {
-                    memo.push(b);
-                    memo.push(b);
-                }
-                a = 1;
-                *memo.last_mut().unwrap() = i;
-            }
-            Ordering::Less => a = -1,
+    input! {_n:usize,s:Chars}
+    let mut ans = true;
+    let mut c = s[0];
+    for i in s.iter().skip(1) {
+        if i == &c {
+            ans = false
         }
-
-        b = i;
+        c = *i
     }
-    let mut ans = 0;
-    let mut i = 0;
-    for hi in memo {
-        if hi >= i {
-            ans += (hi - i).abs();
-        }
-        i = hi;
-    }
-    println!("{}", ans)
+    println!("{}", if ans { "Yes" } else { "No" })
 }
