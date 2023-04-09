b35081b2ddc1c759748ab4d51032f4cd440a8739
tunamaguro <tsu7magu6@gmail.com>
2023-04-09 12:15:00 +0000

    ABC297 B

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {n:usize,d:i32,t:[i32;n]}
-    let mut ans = -1;
-    t.iter().skip(1).fold(t[0], |acc, x| {
-        if ans == -1 && x - acc <= d {
-            ans = *x;
-        }
-        *x
-    });
+    input! {s:Chars}
+    let mut ans = true;
 
-    println!("{}", ans)
+    let mut b1 = -1;
+    for (i, c) in s.iter().enumerate() {
+        if b1 != -1 && c == &'B' {
+            let x = b1;
+            let y = i as i32;
+            ans &= x % 2 != y % 2;
+            break;
+        }
+        if b1 == -1 && c == &'B' {
+            b1 = i as i32;
+        }
+    }
+
+    let mut r = 0;
+    for c in s {
+        if c == 'K' && r != 1 {
+            ans = false;
+            break;
+        }
+        if c == 'R' {
+            r += 1;
+        }
+    }
+
+    println!("{}", if ans { "Yes" } else { "No" })
 }
