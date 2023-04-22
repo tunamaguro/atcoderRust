a3996c8b0b45aa5716120ba7f68c1fb1ea6c2f9c
tunamaguro <tsu7magu6@gmail.com>
2023-04-22 12:03:55 +0000

    ABC299 A

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
-use proconio::input;
-use std::cmp::{max, min};
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {a:i128,b:i128}
-    let mut ans = 0;
-    let mut x = max(a, b);
-    let mut y = min(a, b);
-    // println!("x = {}  y = {}", x, y);
-    while x != y {
-        // println!("x = {}  y = {}  ans = {}", x, y, ans);
-        let div = x / y;
-        ans += div;
-        let a = x % y;
-        let b = y;
-        if a == 0 {
-            ans -= 1;
-            break;
+    input! {_n:usize,s:Chars}
+    let mut ans = false;
+    let mut find = false;
+    for c in s {
+        if c == '|' {
+            find = !find
         }
-        x = max(a, b);
-        y = min(a, b);
+        if find && c == '*' {
+            ans = true
+        }
     }
-    println!("{}", ans)
+
+    println!("{}", if ans { "in" } else { "out" })
 }
