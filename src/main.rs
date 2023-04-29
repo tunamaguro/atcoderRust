589b553b342d9928ea18b5b2182b580d50adba0e
tunamaguro <tsu7magu6@gmail.com>
2023-04-29 12:01:35 +0000

    ABC300 A

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
 use proconio::input;
 
-use proconio::source::line::LineSource;
-use std::io;
-use std::io::BufReader;
-
 fn main() {
-    let mut stdin = LineSource::new(BufReader::new(io::stdin()));
-    macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
+    input! {n:usize,a:usize,b:usize,c:[usize;n]}
 
-    input! {n:i32}
-    let mut left = 0;
-    let mut right = n;
-    for _ in 0..20 {
-        let mid = left + (right - left) / 2;
-        if right - left > 1 {
-            println!("? {}", mid);
-            input! {s:i32}
-            match s {
-                0 => left = mid,
-                1 => right = mid,
-                _ => unreachable!(),
-            }
-        } else {
-            println!("! {}", left);
-            break;
+    for (i, ci) in c.iter().enumerate() {
+        if *ci == (a + b) {
+            println!("{}", i + 1);
+            return;
         }
     }
 }
