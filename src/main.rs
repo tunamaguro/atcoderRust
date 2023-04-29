a920e62016a095a76198390bfbf932287ea03149
tunamaguro <tsu7magu6@gmail.com>
2023-04-29 12:56:53 +0000

    ABC300 C

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,67 @@
+use itertools::Itertools;
 use proconio::{input, marker::Chars};
+use std::cmp::min;
 
 fn main() {
-    input! {h:usize,w:usize, a:[Chars;h],b:[Chars;h]}
-    for si in 0..h {
-        for ti in 0..w {
-            let mut a = a.clone();
-            // 縦方向シフト
-            for _ in 0..si {
-                for wi in 0..w {
-                    let r0 = a[0][wi];
-                    for hi in 0..(h - 1) {
-                        a[hi][wi] = a[hi + 1][wi]
-                    }
-                    a[h - 1][wi] = r0
-                }
-            }
+    input! {h:usize,w:usize,c:[Chars;h]}
+    let mut ans = vec![0; min(h, w)];
+    let mut visited = vec![vec![false; w]; h];
 
-            // 横方向シフト
-            for _ in 0..ti {
-                for hi in 0..h {
-                    let c0 = a[hi][0];
-                    for wi in 0..(w - 1) {
-                        a[hi][wi] = a[hi][wi + 1];
-                    }
-                    a[hi][w - 1] = c0
+    let mut queue: Vec<(usize, usize)> = vec![];
+
+    for hi in 0..h {
+        for wi in 0..w {
+            // すでに訪れていればスキップ
+            if visited[hi][wi] {
+                continue;
+            }
+            if c[hi][wi] != '#' {
+                visited[hi][wi] = true;
+                continue;
+            }
+            let mut edge = 0;
+            queue.push((hi, wi));
+            while !queue.is_empty() {
+                let (nh, nw) = queue.pop().unwrap();
+                // println!(
+                //     "now ({} , {})  visited = {}  queue {:?}",
+                //     nh, nw, visited[nh][nw], queue
+                // );
+                if visited[nh][nw] {
+                    continue;
                 }
+                visited[nh][nw] = true;
+
+                if c[nh][nw] != '#' {
+                    continue;
+                }
+                edge += 1;
+
+                // 斜めに探索する
+                // 左上
+                if 0 < nh && 0 < nw {
+                    queue.push((nh - 1, nw - 1))
+                }
+                // 右上
+                if 0 < nh && nw < w - 1 {
+                    queue.push((nh - 1, nw + 1))
+                }
+                // 左下
+                if nh < h - 1 && 0 < nw {
+                    queue.push((nh + 1, nw - 1))
+                }
+                // 右下
+                if nh < h - 1 && nw < w - 1 {
+                    queue.push((nh + 1, nw + 1))
+                }
+                // println!("edge = {}", edge)
             }
-            if a == b {
-                println!("Yes");
-                return;
-            }
+            let batsu = (edge - 1) / 4;
+            ans[batsu - 1] += 1;
+            // println!("batsu size = {}", batsu)
         }
     }
-    println!("No")
+
+    let ans = ans.iter().map(|x| x.to_string()).join(" ");
+    println!("{}", ans)
 }
