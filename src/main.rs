7bf5be27a4d61c70246e7f61b0a8891b42c4cd5e
tunamaguro <tsu7magu6@gmail.com>
2023-05-14 06:20:35 +0000

    ABC301 A

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,23 @@
-use itertools::Itertools;
 use proconio::{input, marker::Chars};
-use std::cmp::min;
 
 fn main() {
-    input! {h:usize,w:usize,c:[Chars;h]}
-    let mut ans = vec![0; min(h, w)];
-    let mut visited = vec![vec![false; w]; h];
-
-    let mut queue: Vec<(usize, usize)> = vec![];
-
-    for hi in 0..h {
-        for wi in 0..w {
-            // すでに訪れていればスキップ
-            if visited[hi][wi] {
-                continue;
-            }
-            if c[hi][wi] != '#' {
-                visited[hi][wi] = true;
-                continue;
-            }
-            let mut edge = 0;
-            queue.push((hi, wi));
-            while !queue.is_empty() {
-                let (nh, nw) = queue.pop().unwrap();
-                // println!(
-                //     "now ({} , {})  visited = {}  queue {:?}",
-                //     nh, nw, visited[nh][nw], queue
-                // );
-                if visited[nh][nw] {
-                    continue;
-                }
-                visited[nh][nw] = true;
-
-                if c[nh][nw] != '#' {
-                    continue;
-                }
-                edge += 1;
-
-                // 斜めに探索する
-                // 左上
-                if 0 < nh && 0 < nw {
-                    queue.push((nh - 1, nw - 1))
-                }
-                // 右上
-                if 0 < nh && nw < w - 1 {
-                    queue.push((nh - 1, nw + 1))
-                }
-                // 左下
-                if nh < h - 1 && 0 < nw {
-                    queue.push((nh + 1, nw - 1))
-                }
-                // 右下
-                if nh < h - 1 && nw < w - 1 {
-                    queue.push((nh + 1, nw + 1))
-                }
-                // println!("edge = {}", edge)
-            }
-            let batsu = (edge - 1) / 4;
-            ans[batsu - 1] += 1;
-            // println!("batsu size = {}", batsu)
+    input! {n:usize,s:Chars}
+    let mut t = 0;
+    let mut a = 0;
+    let half = n / 2 + n % 2;
+    for si in s {
+        match si {
+            'T' => t += 1,
+            'A' => a += 1,
+            _ => unreachable!(),
+        }
+        if t >= half {
+            println!("T");
+            break;
+        }
+        if a >= half {
+            println!("A");
+            break;
         }
     }
-
-    let ans = ans.iter().map(|x| x.to_string()).join(" ");
-    println!("{}", ans)
 }
