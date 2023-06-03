a64478f4a94891ef889fb5fa3aa3ce04ee724ad8
tunamaguro <tsu7magu6@gmail.com>
2023-06-03 12:26:30 +0000

    ABC304 C

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use proconio::input;
 
 fn main() {
-    input! {n:i32}
-    if n < 10_i32.pow(3) {
-        println!("{}", n)
-    } else if n < 10_i32.pow(4) {
-        println!("{}", n - n % 10_i32.pow(1))
-    } else if n < 10_i32.pow(5) {
-        println!("{}", n - n % 10_i32.pow(2))
-    } else if n < 10_i32.pow(6) {
-        println!("{}", n - n % 10_i32.pow(3))
-    } else if n < 10_i32.pow(7) {
-        println!("{}", n - n % 10_i32.pow(4))
-    } else if n < 10_i32.pow(8) {
-        println!("{}", n - n % 10_i32.pow(5))
-    } else if n < 10_i32.pow(9) {
-        println!("{}", n - n % 10_i32.pow(6))
+    input! {n:usize,d:i32,p:[(i32,i32);n]}
+    let mut has_infected = vec![false; n];
+    let mut queue: Vec<usize> = vec![0];
+    while !queue.is_empty() {
+        let i = queue.pop().unwrap();
+        if has_infected[i] {
+            continue;
+        }
+        has_infected[i] = true;
+        let (x1, y1) = p[i];
+        for (i, (x2, y2)) in p.iter().enumerate() {
+            let dist = (x1 - x2).pow(2) + (y1 - y2).pow(2);
+            if d.pow(2) >= dist {
+                queue.push(i)
+            }
+        }
+    }
+    for ans in has_infected {
+        println!("{}", if ans { "Yes" } else { "No" })
     }
 }
