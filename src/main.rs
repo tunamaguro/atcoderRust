453987e4431f9a18432ba78f5df0e50a1221cf25
tunamaguro <tsu7magu6@gmail.com>
2023-06-10 13:33:22 +0000

    ABC305 D

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,78 @@
-use proconio::{input, marker::Chars};
+use proconio::input;
 
-fn main() {
-    input! {h:usize,w:usize,s:[Chars;h]}
-    for hi in 0..h {
-        for wi in 0..w {
-            if s[hi][wi] != '.' {
-                continue;
-            }
+trait Bound<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+    fn upper_bound(&self, x: &T) -> usize;
+}
 
-            let mut c = 0;
-            if hi > 0 && s[hi - 1][wi] == '#' {
-                c += 1;
-            }
-            if hi < h - 1 && s[hi + 1][wi] == '#' {
-                c += 1;
-            }
-            if wi > 0 && s[hi][wi - 1] == '#' {
-                c += 1;
-            }
-            if wi < w - 1 && s[hi][wi + 1] == '#' {
-                c += 1;
-            }
-            if c >= 2 {
-                println!("{} {}", hi + 1, wi + 1);
-                return;
+impl<T: PartialOrd> Bound<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let (mut low, mut high) = (0, self.len());
+        while low + 1 < high {
+            let mid = (low + high) / 2;
+            if self[mid] < *x {
+                low = mid;
+            } else {
+                high = mid;
             }
         }
+        if self[low] < *x {
+            low + 1
+        } else {
+            low
+        }
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let (mut low, mut high) = (0, self.len());
+        while low + 1 < high {
+            let mid = (low + high) / 2;
+            if self[mid] <= *x {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        if self[low] <= *x {
+            low + 1
+        } else {
+            low
+        }
+    }
+}
+
+fn main() {
+    input! {n:usize,a:[usize;n],q:usize,query:[(usize,usize);q]}
+    let mut memo = vec![0];
+    for (i, v) in a.iter().enumerate().skip(1) {
+        let last = memo.last().unwrap();
+        if i % 2 == 0 {
+            let sleep = v - a[i - 1];
+            memo.push(last + sleep)
+        } else {
+            memo.push(*last)
+        }
+    }
+    // println!("{:?}", memo);
+    for (l, r) in query {
+        let left = a.lower_bound(&l);
+        let right = a.lower_bound(&r);
+        // println!("l: {}  r: {}", l, r);
+        // println!(
+        //     "left: {} ({}) right: {} ({})",
+        //     left, a[left], right, a[right]
+        // );
+
+        let mut ans = memo[right] - memo[left];
+        if left % 2 == 0 {
+            // println!("left tyousei");
+            ans += a[left] - l
+        }
+        if right % 2 == 0 {
+            // println!("right tyousei");
+            ans -= a[right] - r;
+        }
+
+        println!("{}", ans)
     }
 }
