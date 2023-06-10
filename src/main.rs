7fccce22ccbc4fa3479a28930189baec4f631c8c
tunamaguro <tsu7magu6@gmail.com>
2023-06-10 12:07:06 +0000

    ABC305 A

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,15 @@
+
 use proconio::input;
 
 fn main() {
-    input! {n:usize,d:i32,p:[(i32,i32);n]}
-    let mut has_infected = vec![false; n];
-    let mut queue: Vec<usize> = vec![0];
-    while !queue.is_empty() {
-        let i = queue.pop().unwrap();
-        if has_infected[i] {
-            continue;
+    input! {n:i32}
+    let water: Vec<_> = (0..21).map(|x| x * 5).collect();
+    let ans = water.iter().fold(1000, |acc, x| {
+        if (n - acc).abs() > (n - x).abs() {
+            *x
+        } else {
+            acc
         }
-        has_infected[i] = true;
-        let (x1, y1) = p[i];
-        for (i, (x2, y2)) in p.iter().enumerate() {
-            let dist = (x1 - x2).pow(2) + (y1 - y2).pow(2);
-            if d.pow(2) >= dist {
-                queue.push(i)
-            }
-        }
-    }
-    for ans in has_infected {
-        println!("{}", if ans { "Yes" } else { "No" })
-    }
+    });
+    println!("{}", ans)
 }
