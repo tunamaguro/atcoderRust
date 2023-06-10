c4f5903dc76fe73b2a5b4ef3cbf85e342fc1ea4c
tunamaguro <tsu7magu6@gmail.com>
2023-06-10 12:19:55 +0000

    ABC305 B

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-
 use proconio::input;
 
 fn main() {
-    input! {n:i32}
-    let water: Vec<_> = (0..21).map(|x| x * 5).collect();
-    let ans = water.iter().fold(1000, |acc, x| {
-        if (n - acc).abs() > (n - x).abs() {
-            *x
-        } else {
-            acc
-        }
-    });
-    println!("{}", ans)
+    input! {p:char,q:char}
+    let mut a = [p, q];
+    a.sort();
+    let [p, q] = a;
+    let alpha: Vec<_> = "ABCDEFG".chars().collect();
+    let distance = [3, 1, 4, 1, 5, 9];
+    let pi = alpha.iter().position(|&x| x == p).unwrap();
+    let qi = alpha.iter().position(|&x| x == q).unwrap();
+    let ans: i32 = distance[pi..qi].iter().sum();
+    println!("{}", ans);
 }
