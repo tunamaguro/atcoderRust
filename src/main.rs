9b810365b19eb41ecee541d33ca952d71d7edf19
tunamaguro <tsu7magu6@gmail.com>
2023-06-10 12:30:28 +0000

    ABC305 C

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {p:char,q:char}
-    let mut a = [p, q];
-    a.sort();
-    let [p, q] = a;
-    let alpha: Vec<_> = "ABCDEFG".chars().collect();
-    let distance = [3, 1, 4, 1, 5, 9];
-    let pi = alpha.iter().position(|&x| x == p).unwrap();
-    let qi = alpha.iter().position(|&x| x == q).unwrap();
-    let ans: i32 = distance[pi..qi].iter().sum();
-    println!("{}", ans);
+    input! {h:usize,w:usize,s:[Chars;h]}
+    for hi in 0..h {
+        for wi in 0..w {
+            if s[hi][wi] != '.' {
+                continue;
+            }
+
+            let mut c = 0;
+            if hi > 0 && s[hi - 1][wi] == '#' {
+                c += 1;
+            }
+            if hi < h - 1 && s[hi + 1][wi] == '#' {
+                c += 1;
+            }
+            if wi > 0 && s[hi][wi - 1] == '#' {
+                c += 1;
+            }
+            if wi < w - 1 && s[hi][wi + 1] == '#' {
+                c += 1;
+            }
+            if c >= 2 {
+                println!("{} {}", hi + 1, wi + 1);
+                return;
+            }
+        }
+    }
 }
