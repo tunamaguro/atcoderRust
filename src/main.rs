032d3d01577129e65ad89fea85abf0f153d4ba83
tunamaguro <tsu7magu6@gmail.com>
2023-06-17 12:02:14 +0000

    ABC306 A

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 trait Bound<T> {
     fn lower_bound(&self, x: &T) -> usize;
@@ -42,37 +42,9 @@ impl<T: PartialOrd> Bound<T> for [T] {
 }
 
 fn main() {
-    input! {n:usize,a:[usize;n],q:usize,query:[(usize,usize);q]}
-    let mut memo = vec![0];
-    for (i, v) in a.iter().enumerate().skip(1) {
-        let last = memo.last().unwrap();
-        if i % 2 == 0 {
-            let sleep = v - a[i - 1];
-            memo.push(last + sleep)
-        } else {
-            memo.push(*last)
-        }
+    input! {_n:usize,s:Chars}
+    for si in s {
+        print!("{}{}", si, si);
     }
-    // println!("{:?}", memo);
-    for (l, r) in query {
-        let left = a.lower_bound(&l);
-        let right = a.lower_bound(&r);
-        // println!("l: {}  r: {}", l, r);
-        // println!(
-        //     "left: {} ({}) right: {} ({})",
-        //     left, a[left], right, a[right]
-        // );
-
-        let mut ans = memo[right] - memo[left];
-        if left % 2 == 0 {
-            // println!("left tyousei");
-            ans += a[left] - l
-        }
-        if right % 2 == 0 {
-            // println!("right tyousei");
-            ans -= a[right] - r;
-        }
-
-        println!("{}", ans)
-    }
+    println!()
 }
