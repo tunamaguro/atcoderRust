4ef2452b44876daf3489974d3da061309abce468
tunamaguro <tsu7magu6@gmail.com>
2024-05-15 14:27:07 +0000

    ABC278 E

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
 use proconio::input;
+use std::{collections::BTreeMap, vec};
 
 trait Bound<T> {
     fn lower_bound(&self, x: &T) -> usize;
@@ -43,37 +44,50 @@ impl<T: PartialOrd> Bound<T> for [T] {
 }
 
 fn main() {
-    input! {n:usize,a:[usize;n]}
-    let a = a.into_iter().sorted().collect_vec();
-    let mut acc = vec![0];
-    for ai in &a {
-        acc.push(ai + acc.last().unwrap());
-    }
+    input! {h:usize,w:usize,n:usize,mh:usize,mw:usize,mut grid:[[usize;w];h]}
 
-    let e = 10_usize.pow(8);
-
-    let mut ans = 0;
-    for i in 0..n - 1 {
-        let ai = a[i];
-        let ajs = &a[(i + 1)..];
-        ans += ai * ajs.len();
-        ans += acc.last().unwrap() - acc[i + 1];
-        // println!("ans = {}", ans);
-        if ai < e {
-            let lower_10e8 = ajs.lower_bound(&(e - ai));
-            // #[cfg(debug_assertions)]
-            // {
-            //     println!("ai: {}, ajs: {:?}, lower_10e8: {}", ai, ajs, lower_10e8);
-            //     println!(
-            //         "ajs.len: {}, num of over 10e8(a+b): {}",
-            //         ajs.len(),
-            //         ajs.len() - lower_10e8
-            //     );
-            // }
-            ans -= e * (ajs.len() - lower_10e8)
-        } else {
-            ans -= e * ajs.len();
+    let mut count_set = vec![vec![vec![0; n]; w]; h];
+    for hi in 0..h {
+        for wi in 0..w {
+            let v = grid[hi][wi] - 1;
+            count_set[hi][wi][v] += 1;
+            for ni in 0..n {
+                if hi > 0 {
+                    count_set[hi][wi][ni] += count_set[hi - 1][wi][ni];
+                }
+                if wi > 0 {
+                    count_set[hi][wi][ni] += count_set[hi][wi - 1][ni];
+                }
+                if hi > 0 && wi > 0 {
+                    count_set[hi][wi][ni] -= count_set[hi - 1][wi - 1][ni];
+                }
+            }
         }
     }
-    println!("{}", ans);
+
+    for k in 0..=h - mh {
+        for l in 0..=w - mw {
+            let mut count = 0;
+            // println!("k: {}, l: {}", k, l);
+            let mh = mh - 1;
+            let mw = mw - 1;
+            for ni in 0..n {
+                let mut num_ = count_set[h - 1][w - 1][ni] - count_set[k + mh][l + mw][ni];
+                if k > 0 {
+                    num_ += count_set[k - 1][l + mw][ni];
+                }
+                if l > 0 {
+                    num_ += count_set[k + mh][l - 1][ni];
+                }
+                if k > 0 && l > 0 {
+                    num_ -= count_set[k - 1][l - 1][ni];
+                }
+                if num_ > 0 {
+                    count += 1;
+                }
+            }
+            print!("{} ", count);
+        }
+        println!();
+    }
 }
