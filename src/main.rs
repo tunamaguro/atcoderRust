8b773ca7acca5e3cb5f3b04328d24c7d90cad9a4
tunamaguro <tsu7magu6@gmail.com>
2024-05-25 12:35:42 +0000

    ABC355 C

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use proconio::input;
 
 trait Bound<T> {
@@ -42,13 +44,70 @@ impl<T: PartialOrd> Bound<T> for [T] {
 }
 
 fn main() {
-    input! {n:usize,m:usize,mut a:[i32;n],_b:[i32;m]}
-    a.sort();
-    let mut ans = false;
-    for (a1, a2) in a.iter().zip(a.iter().skip(1)) {
-        if a2 - a1 == 1 {
-            ans = true
+    input! {n:usize,t:usize,a:[usize;t]}
+
+    let mut memo = BTreeMap::new();
+    for (ti, ai) in a.iter().enumerate() {
+        memo.insert(ai - 1, ti + 1);
+    }
+
+    let mut min_bingo_time = 10000000;
+    // 縦列のビンゴがあったか判定する
+    for row in 0..n {
+        let mut called_time = vec![];
+        for col in 0..n {
+            let pos = n * row + col;
+            if let Some(c) = memo.get(&pos) {
+                called_time.push(c)
+            }
+        }
+        if called_time.len() == n {
+            let min_t = called_time.iter().max().unwrap();
+            min_bingo_time = min_bingo_time.min(**min_t);
         }
     }
-    println!("{}", if ans { "Yes" } else { "No" })
+    // 横列のビンゴがあったか判定する
+    for col in 0..n {
+        let mut called_time = vec![];
+        for row in 0..n {
+            let pos = n * row + col;
+            if let Some(c) = memo.get(&pos) {
+                called_time.push(c)
+            }
+        }
+        if called_time.len() == n {
+            let min_t = called_time.iter().max().unwrap();
+            min_bingo_time = min_bingo_time.min(**min_t);
+        }
+    }
+    // 斜めのビンゴがあったか判定する
+    let mut rd_called_time = vec![]; // 左上->右下
+    let mut ld_called_time = vec![]; // 右上->左下
+    for i in 0..n {
+        let rd_pos = n * i + i;
+        let ld_pos = n * i + n - i;
+        if let Some(c) = memo.get(&rd_pos) {
+            rd_called_time.push(c)
+        }
+        if let Some(c) = memo.get(&ld_pos) {
+            ld_called_time.push(c)
+        }
+    }
+    if rd_called_time.len() == n {
+        let min_t = rd_called_time.iter().max().unwrap();
+        min_bingo_time = min_bingo_time.min(**min_t);
+    }
+    if ld_called_time.len() == n {
+        let min_t = ld_called_time.iter().max().unwrap();
+        min_bingo_time = min_bingo_time.min(**min_t);
+    }
+
+    println!(
+        "{}",
+        if min_bingo_time < 10000000 {
+            format!("{}", min_bingo_time)
+        } else {
+            "-1".to_string()
+        }
+    )
 }
