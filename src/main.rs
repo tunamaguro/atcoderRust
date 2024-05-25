4c90ea9b30a67080c2de8062ad45c732383ac3fa
tunamaguro <tsu7magu6@gmail.com>
2024-05-25 12:03:28 +0000

    ABC355 A

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
 use proconio::input;
 
 trait Bound<T> {
@@ -42,67 +45,14 @@ impl<T: PartialOrd> Bound<T> for [T] {
 }
 
 fn main() {
-    input! {a:i64,b:i64,c:i64,d:i64}
+    input! {a:i32,b:i32}
+    let mut hannin = HashSet::from([1, 2, 3]);
+    hannin.remove(&a);
+    hannin.remove(&b);
 
-    let h = d - b;
-    let w = c - a;
-    let area = [(2, 1), (1, 2), (0, 1), (1, 0)];
-    let mut cur_x = a;
-    let mut ans = 0;
-
-    let left_idx = (a + 10_i64.pow(9)) % 4;
-    let is_odd = b % 2 == 0;
-
-    let count = (w + 3) / 4;
-    let tmp = area[left_idx as usize];
-    let mut col_size = 0;
-    if is_odd {
-        col_size += tmp.0 * ((h + 1) / 2); // １段目
-        col_size += tmp.1 * (h / 2); // 2段目
-    } else {
-        col_size += tmp.1 * ((h + 1) / 2); // １段目
-        col_size += tmp.0 * (h / 2); // 2段目
+    if hannin.len()==1{
+        println!("{}",hannin.iter().collect_vec()[0])
+    }else{
+        println!("-1")
     }
-    ans += col_size * count;
-
-    let next_idx = (left_idx + 1) % 4;
-    let count = (w + 2) / 4;
-    let tmp = area[next_idx as usize];
-    let mut col_size = 0;
-    if is_odd {
-        col_size += tmp.0 * ((h + 1) / 2); // １段目
-        col_size += tmp.1 * (h / 2); // 2段目
-    } else {
-        col_size += tmp.1 * ((h + 1) / 2); // １段目
-        col_size += tmp.0 * (h / 2); // 2段目
-    }
-    ans += col_size * count;
-
-    let next_idx = (next_idx + 1) % 4;
-    let count = (w + 1) / 4;
-    let tmp = area[next_idx as usize];
-    let mut col_size = 0;
-    if is_odd {
-        col_size += tmp.0 * ((h + 1) / 2); // １段目
-        col_size += tmp.1 * (h / 2); // 2段目
-    } else {
-        col_size += tmp.1 * ((h + 1) / 2); // １段目
-        col_size += tmp.0 * (h / 2); // 2段目
-    }
-    ans += col_size * count;
-
-    let next_idx = (next_idx + 1) % 4;
-    let count = (w) / 4;
-    let tmp = area[next_idx as usize];
-    let mut col_size = 0;
-    if is_odd {
-        col_size += tmp.0 * ((h + 1) / 2); // １段目
-        col_size += tmp.1 * (h / 2); // 2段目
-    } else {
-        col_size += tmp.1 * ((h + 1) / 2); // １段目
-        col_size += tmp.0 * (h / 2); // 2段目
-    }
-    ans += col_size * count;
-
-    println!("{}", ans)
 }
