2666a6678d46c36df1c0f73567cb88f45af5d87e
tunamaguro <tsu7magu6@gmail.com>
2024-05-25 13:05:57 +0000

    ABC355 C

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::collections::HashSet;
+use std::collections::BTreeMap;
 
-use itertools::Itertools;
 use proconio::input;
 
 trait Bound<T> {
@@ -45,17 +44,83 @@ impl<T: PartialOrd> Bound<T> for [T] {
 }
 
 fn main() {
-    input! {n:usize,m:usize,mut a:[i32;n],mut b:[i32;m]}
-    let mut memo = HashSet::new();
-    for &ai in a.iter() {
-        memo.insert(ai);
+    input! {n:usize,t:usize,a:[usize;t]}
+
+    let mut memo = BTreeMap::new();
+    for (ti, ai) in a.iter().enumerate() {
+        memo.insert(ai - 1, ti + 1);
     }
-    let mut ans = false;
-    let c = a.into_iter().chain(b.into_iter()).sorted().collect_vec();
-    for (c1, c2) in c.iter().zip(c.iter().skip(1)) {
-        if memo.contains(c1) && memo.contains(c2) {
-            ans = true
+    let mut min_bingo_time = usize::MAX;
+    // 横列のビンゴがあったか判定する
+    'outer: for row in 0..n {
+        let mut min_t = 0;
+        for col in 0..n {
+            let pos = n * row + col;
+            dbg!(format!("row = {}, col = {}, pos = {}", row, col, pos));
+            if let Some(c) = memo.get(&pos) {
+                min_t = min_t.max(*c);
+            } else {
+                dbg!(format!(
+                    "row = {}, col = {}, pos = {} was skip",
+                    row, col, pos
+                ));
+                continue 'outer;
+            }
+        }
+        dbg!(row, min_t);
+        min_bingo_time = min_bingo_time.min(min_t);
+    }
+
+    // 縦列のビンゴがあったか判定する
+    'outer2: for col in 0..n {
+        let mut min_t = 0;
+        for row in 0..n {
+            let pos = n * row + col;
+            dbg!(format!("row = {}, col = {}, pos = {}", row, col, pos));
+            if let Some(c) = memo.get(&pos) {
+                min_t = min_t.max(*c);
+            } else {
+                dbg!(format!(
+                    "row = {}, col = {}, pos = {} was skip",
+                    row, col, pos
+                ));
+                continue 'outer2;
+            }
+        }
+        dbg!(col, min_t);
+        min_bingo_time = min_bingo_time.min(min_t);
+    }
+
+    // 斜めのビンゴがあったか判定する
+    let mut rd_min_t = 0;
+    let mut ld_min_t = 0;
+    for i in 0..n {
+        let rd_pos = n * i + i;
+        let ld_pos = n * i + n - i - 1;
+        dbg!(format!(
+            "i = {}, rd_pos = {}, ld_pos = {}",
+            i, rd_pos, ld_pos
+        ));
+        if let Some(c) = memo.get(&rd_pos) {
+            rd_min_t = rd_min_t.max(*c);
+        }
+        if let Some(c) = memo.get(&ld_pos) {
+            ld_min_t = ld_min_t.max(*c);
         }
     }
-    println!("{}", if ans { "Yes" } else { "No" })
+    if rd_min_t!=0{
+        min_bingo_time=min_bingo_time.min(rd_min_t);
+    }
+    if ld_min_t!=0{
+        min_bingo_time=min_bingo_time.min(ld_min_t);
+    }
+
+    println!(
+        "{}",
+        if min_bingo_time < 10000000 {
+            format!("{}", min_bingo_time)
+        } else {
+            "-1".to_string()
+        }
+    )
 }
