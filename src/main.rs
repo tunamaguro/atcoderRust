2757a10d051d730eb01aa48623a9aa6a73dec3e1
tunamaguro <tsu7magu6@gmail.com>
2024-05-25 12:39:53 +0000

    ABC355 B

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::collections::BTreeMap;
+use std::collections::HashSet;
 
+use itertools::Itertools;
 use proconio::input;
 
 trait Bound<T> {
@@ -44,70 +45,17 @@ impl<T: PartialOrd> Bound<T> for [T] {
 }
 
 fn main() {
-    input! {n:usize,t:usize,a:[usize;t]}
-
-    let mut memo = BTreeMap::new();
-    for (ti, ai) in a.iter().enumerate() {
-        memo.insert(ai - 1, ti + 1);
+    input! {n:usize,m:usize,mut a:[i32;n],mut b:[i32;m]}
+    let mut memo = HashSet::new();
+    for &ai in a.iter() {
+        memo.insert(ai);
     }
-
-    let mut min_bingo_time = 10000000;
-    // 縦列のビンゴがあったか判定する
-    for row in 0..n {
-        let mut called_time = vec![];
-        for col in 0..n {
-            let pos = n * row + col;
-            if let Some(c) = memo.get(&pos) {
-                called_time.push(c)
-            }
-        }
-        if called_time.len() == n {
-            let min_t = called_time.iter().max().unwrap();
-            min_bingo_time = min_bingo_time.min(**min_t);
+    let mut ans = false;
+    let c = a.into_iter().chain(b.into_iter()).sorted().collect_vec();
+    for (c1, c2) in c.iter().zip(c.iter().skip(1)) {
+        if memo.contains(c1) && memo.contains(c2) {
+            ans = true
         }
     }
-    // 横列のビンゴがあったか判定する
-    for col in 0..n {
-        let mut called_time = vec![];
-        for row in 0..n {
-            let pos = n * row + col;
-            if let Some(c) = memo.get(&pos) {
-                called_time.push(c)
-            }
-        }
-        if called_time.len() == n {
-            let min_t = called_time.iter().max().unwrap();
-            min_bingo_time = min_bingo_time.min(**min_t);
-        }
-    }
-    // 斜めのビンゴがあったか判定する
-    let mut rd_called_time = vec![]; // 左上->右下
-    let mut ld_called_time = vec![]; // 右上->左下
-    for i in 0..n {
-        let rd_pos = n * i + i;
-        let ld_pos = n * i + n - i;
-        if let Some(c) = memo.get(&rd_pos) {
-            rd_called_time.push(c)
-        }
-        if let Some(c) = memo.get(&ld_pos) {
-            ld_called_time.push(c)
-        }
-    }
-    if rd_called_time.len() == n {
-        let min_t = rd_called_time.iter().max().unwrap();
-        min_bingo_time = min_bingo_time.min(**min_t);
-    }
-    if ld_called_time.len() == n {
-        let min_t = ld_called_time.iter().max().unwrap();
-        min_bingo_time = min_bingo_time.min(**min_t);
-    }
-
-    println!(
-        "{}",
-        if min_bingo_time < 10000000 {
-            format!("{}", min_bingo_time)
-        } else {
-            "-1".to_string()
-        }
-    )
+    println!("{}", if ans { "Yes" } else { "No" })
 }
