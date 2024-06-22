8266026e226c9a0bd1cb564cd0aa2d135d09667e
tunamaguro <tsu7magu6@gmail.com>
2024-06-22 03:13:16 +0000

    ABC218 C

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use itertools::Itertools;
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 trait Bound<T> {
     fn lower_bound(&self, x: &T) -> usize;
@@ -42,33 +41,65 @@ impl<T: PartialOrd> Bound<T> for [T] {
     }
 }
 
+trait Rotate {
+    fn rotate90(&self) -> Self;
+}
+impl<T: Copy + Default> Rotate for Vec<Vec<T>> {
+    fn rotate90(&self) -> Self {
+        let row = self.len();
+        let col = self.get(0).unwrap().len();
+        let mut result = vec![vec![T::default(); col]; row];
+
+        for i in 0..row {
+            for j in 0..col {
+                let ii = row - i - 1;
+                result[j][ii] = self[i][j];
+            }
+        }
+
+        result
+    }
+}
+
 fn main() {
-    input! {n:usize,m:usize,mut takahashi:[(usize,usize);m],mut aoki:[(usize,usize);m]}
-    let mut a_map = vec![vec![false; n]; n];
-    let mut b_map = vec![vec![false; n]; n];
-    for (a, b) in takahashi {
-        let (a, b) = (a - 1, b - 1);
-        a_map[a][b] = true;
-        a_map[b][a] = true;
-    }
-    for (c, d) in aoki {
-        let (c, d) = (c - 1, d - 1);
-        b_map[c][d] = true;
-        b_map[d][c] = true;
-    }
+    input! {n:usize,s:[Chars;n],t_orig:[Chars;n]}
     let mut ans = false;
-    for p in (0..n).permutations(n) {
-        let mut is_same = true;
+    for rotate in 0..4 {
+        // println!("-------------------------------");
+        let mut t = t_orig.clone();
+        for _ in 0..rotate {
+            t = t.rotate90();
+        }
+
+        let mut s_top_left = (n, n);
+        let mut t_top_left = (n, n);
         for i in 0..n {
             for j in 0..n {
-                if a_map[i][j] != b_map[p[i]][p[j]] {
-                    is_same = false;
+                if s[i][j] == '#' {
+                    s_top_left = (s_top_left.0.min(i), s_top_left.1.min(j));
+                }
+                if t[i][j] == '#' {
+                    t_top_left = (t_top_left.0.min(i), t_top_left.1.min(j));
+                }
+                // print!("{}", t[i][j]);
+            }
+            // println!();
+        }
+        // ずらした後の位置
+        let mut ss = vec![vec![false; n]; n];
+        let mut tt = vec![vec![false; n]; n];
+        for i in 0..n {
+            for j in 0..n {
+                if s[i][j] == '#' {
+                    ss[i - s_top_left.0][j - s_top_left.1] = true;
+                }
+                if t[i][j] == '#' {
+                    tt[i - t_top_left.0][j - t_top_left.1] = true;
                 }
             }
         }
-        if is_same {
-            ans = true;
-        }
+
+        ans |= ss == tt;
     }
     println!("{}", if ans { "Yes" } else { "No" })
 }
