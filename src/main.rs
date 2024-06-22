d1817d1de43932e91d2a13b87e8772bd32b7933c
tunamaguro <tsu7magu6@gmail.com>
2024-06-22 12:02:23 +0000

    ABC359 A

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use proconio::{input, marker::Chars};
+use proconio::input;
 
 trait Bound<T> {
     fn lower_bound(&self, x: &T) -> usize;
@@ -41,65 +41,8 @@ impl<T: PartialOrd> Bound<T> for [T] {
     }
 }
 
-trait Rotate {
-    fn rotate90(&self) -> Self;
-}
-impl<T: Copy + Default> Rotate for Vec<Vec<T>> {
-    fn rotate90(&self) -> Self {
-        let row = self.len();
-        let col = self.get(0).unwrap().len();
-        let mut result = vec![vec![T::default(); col]; row];
-
-        for i in 0..row {
-            for j in 0..col {
-                let ii = row - i - 1;
-                result[j][ii] = self[i][j];
-            }
-        }
-
-        result
-    }
-}
-
 fn main() {
-    input! {n:usize,s:[Chars;n],t_orig:[Chars;n]}
-    let mut ans = false;
-    for rotate in 0..4 {
-        // println!("-------------------------------");
-        let mut t = t_orig.clone();
-        for _ in 0..rotate {
-            t = t.rotate90();
-        }
-
-        let mut s_top_left = (n, n);
-        let mut t_top_left = (n, n);
-        for i in 0..n {
-            for j in 0..n {
-                if s[i][j] == '#' {
-                    s_top_left = (s_top_left.0.min(i), s_top_left.1.min(j));
-                }
-                if t[i][j] == '#' {
-                    t_top_left = (t_top_left.0.min(i), t_top_left.1.min(j));
-                }
-                // print!("{}", t[i][j]);
-            }
-            // println!();
-        }
-        // ずらした後の位置
-        let mut ss = vec![vec![false; n]; n];
-        let mut tt = vec![vec![false; n]; n];
-        for i in 0..n {
-            for j in 0..n {
-                if s[i][j] == '#' {
-                    ss[i - s_top_left.0][j - s_top_left.1] = true;
-                }
-                if t[i][j] == '#' {
-                    tt[i - t_top_left.0][j - t_top_left.1] = true;
-                }
-            }
-        }
-
-        ans |= ss == tt;
-    }
-    println!("{}", if ans { "Yes" } else { "No" })
+    input! {n:usize,s:[String;n]}
+    let ans = s.iter().filter(|ss| **ss == "Takahashi").count();
+    println!("{}", ans)
 }
