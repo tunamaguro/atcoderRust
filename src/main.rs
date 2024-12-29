782b7e0c29b0d270d3c99afc14817a98019e9418
tunamaguro <tsu7magu6@gmail.com>
2024-12-29 05:12:54 +0000

    ABC386 E
    答えを見て回答

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![allow(dead_code)]
 
-use itertools::Itertools;
 use proconio::input;
 
 trait Bound<T> {
@@ -44,19 +43,41 @@ impl<T: PartialOrd> Bound<T> for [T] {
     }
 }
 
-fn main() {
-    input! {_n:usize,m:usize,colors:[(i32,i32,char);m]}
-
-    let colors = colors.into_iter().sorted().collect::<Vec<_>>();
-
-    let mut min_y = i32::MAX;
-    for (_x, y, c) in colors {
-        if c == 'W' {
-            min_y = min_y.min(y);
-        } else if y >= min_y {
-            println!("No");
-            return;
-        }
+fn func(a: &[usize], c: usize, current_xor: usize, ans: &mut usize) {
+    // c = 0(残り選択数が 0) ならば、最大値を更新して戻る
+    if c == 0 {
+        *ans = current_xor.max(*ans);
+        return;
     }
-    println!("Yes")
+    // idx が配列の長さに達したら探索を終了
+    if a.is_empty() {
+        return;
+    }
+    if let Some((first, rest)) = a.split_first() {
+        // A[idx] を使う場合
+        func(rest, c - 1, current_xor ^ first, ans);
+        // A[idx] を使わない場合
+        func(rest, c, current_xor, ans);
+    }
+}
+
+fn main() {
+    input! {n:usize,k:usize,a:[usize;n]}
+
+    let mut ans = 0;
+
+    if k <= n - k {
+        func(&a, k, 0, &mut ans);
+    } else {
+        let mut all_xor = 0;
+        for &val in &a {
+            all_xor ^= val;
+        }
+        // XORは可逆性があるので、xorを取り直すと元の値に戻る
+        // a xor b = c
+        // c xor b = a
+        func(&a, n - k, all_xor, &mut ans);
+    }
+
+    println!("{}", ans)
 }
